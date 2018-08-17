5efe7d7f25e818c1c1242d034d97a896f8b8bf2f
Charlie Jung <32102793+Charlanx@users.noreply.github.com>
2018-08-17 13:01:39 -0500

    second commit

--- lint.py
+++ lint.py
@@ -6,32 +6,32 @@ if sys.version_info[0] == 2:
             raise ValueError("Empty String")
     except ValueError as e:
         print(e)
-    if sys.version_info[0] == 3:
-        try:
-            user_input = input("Please enter the file you would like to use: ")
-            if not user_input:
-                 raise ValueError("Empty String")
-        except ValueError as e:
-            print(e)
+if sys.version_info[0] == 3:
+    try:
+        user_input = input("Please enter the file you would like to use: ")
+        if not user_input:
+            raise ValueError("Empty String")
+    except ValueError as e:
+        print(e)
 filePath = user_input 
-try:
-    chosenChar = input("Input the character you would like to check: ")
-
-    with open(filePath) as fp:
-        line = fp.readline()
-        if line == "":
-            print("File is empty. Terminating program.")
-            sys.exit()
+chosenChar = input("Input the character you would like to check: ")
+print("File name: " + filePath)
+with open(filePath, "r+") as fp:
+    line = fp.readline()
+    if line == "":
+        print("File is empty. Terminating program.")
+        sys.exit(0)
     count = 1
     while line:
         print("Line " + str(count) + ": " + line)
         numOfChars = line.count(chosenChar)
-        if numOfChars > 0:
-            while line.count(chosenChar) > 1:
-                startingChar = 0
-                endingChar = len(line)
-        if line.find(chosenChar, startingChar, endingChar) != -1: 
-             line.find(chosenChar, line.find(chosenChar)+line.count(chosenChar))
-             tempString = line[:line.find(chosenChar)+line.count(chosenChar)] + line[:line.find(chosenChar)+line.count(chosenChar)+1]
+        print("This line has " + str(numOfChars) + "matches!")
+        if numOfChars > 1:
+            startingChar = 0
+            endingChar = len(line)
+            if line.find(chosenChar, startingChar, endingChar) != -1: 
+                tempString = line[:(line.find(chosenChar) + 1)] 
+                print("TempString: " + tempString)
+                fp.write(tempString)
         line = fp.readline()
         count += 1
