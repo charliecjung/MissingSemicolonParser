a052f3fcd1a568649d4d99d941f3070bb95462d2
Charlie Jung <32102793+Charlanx@users.noreply.github.com>
2018-08-19 04:35:47 -0500

    Updated a feature where you can add file as a cmd line argument and choose a certain character to not duplicate (e.g. ;)

--- lint.py
+++ lint.py
@@ -1,32 +1,34 @@
 import sys
-if sys.version_info[0] == 2:
-    try:
-        user_input = raw_input("Please enter the file you would like to use: ")
-        if not user_input:
-            raise ValueError("Empty String")
-    except ValueError as e:
-        print(e)
-if sys.version_info[0] == 3:
-    try:
-        user_input = input("Please enter the file you would like to use: ")
-        if not user_input:
-            raise ValueError("Empty String")
-    except ValueError as e:
-        print(e)
-filePath = user_input 
-#chosenChar = input("Input the character you would like to check: ")
+filePath = ""
+if len(sys.argv) == 2:
+    filePath = sys.argv[1]
+if len(sys.argv) == 1:
+    if sys.version_info[0] == 2:
+        try:
+            user_input = raw_input("Please enter the file you would like to use: ")
+            if not user_input:
+                raise ValueError("Empty String")
+        except ValueError as e:
+            print(e)
+    if sys.version_info[0] == 3:
+        try:
+            user_input = input("Please enter the file you would like to use: ")
+            if not user_input:
+                raise ValueError("Empty String")
+        except ValueError as e:
+            print(e)
+    filePath = user_input 
+chosenChar = input("Input the character you would like to check: ")
 print("File name: " + filePath)
 fileData = ""
 finalMessage = ""
 with open(filePath, "r") as fp:
     fileData = fp.readlines()
     for i in range(len(fileData)):
-        print(fileData[i])
-    for i in range(len(fileData)):
         startingChar = 0
         while startingChar < len(fileData[i])-1:
-            if (fileData[i][startingChar] == fileData[i][startingChar+1]): 
-                    chosenChar = fileData[i][startingChar]
+            if fileData[i][startingChar] == chosenChar:
+                if fileData[i][startingChar+1] == chosenChar:
                     count = 0
                     currentPosition = startingChar
                     while (fileData[i][currentPosition] == chosenChar):
@@ -38,4 +40,4 @@ with open(filePath, "r") as fp:
 with open(filePath, "w") as fp:
     for i in range(len(fileData)):
         fp.write(fileData[i])
-        
+    print("We have stripped duplicating characters from " + filePath + ". We hope to see you again! :)")    
