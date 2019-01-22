aba3c0811e4230c18a2edf44208295c000a87180
Pradyumna Shome <pradyumna.shome@gmail.com>
2019-01-22 00:16:57 -0600

    Removed commented code.

--- lint.c
+++ lint.c
@@ -25,25 +25,3 @@ int main(int argc, char** argv) {
     }
     myFile.close();
 }
-/*
-
-
-string line;
-ifstream myfile ("example.txt");
-if (myfile.is_open())
-{
-  while ( getline (myfile, line))
-{
-  cout << line << "\n";
-}
-else cout << "unable to open
-myfile.close();
-return 0;
-std::cout << "Hello";
-    std::string userInput;
-    std::cin >> userInput;
-    std::cout << userInput;
-    r
-
-
-*/
